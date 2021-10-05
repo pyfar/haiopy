71797a1e6822a01bc292b4aecf107fe9781597dd
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-10-05 15:43:06 +0200

    drafting

---
+++ haiopy/devices.py
@@ -0,0 +1,96 @@
+import sounddevice as sd
+
+
+def list_devices():
+    pass
+
+
+class _Device(object):
+    def __init__(
+            self,
+            id,
+            sampling_rate,
+            block_size,
+            dtype):
+        super().__init__()
+
+    @property
+    def name(self):
+        raise NotImplementedError('Abstract method')
+
+    def playback():
+        pass
+
+    def record():
+        pass
+
+    def playback_record():
+        pass
+
+    def initialize_playback():
+        pass
+
+    def initialize_record():
+        pass
+
+    def initialize_playback_record():
+        pass
+
+    def abort():
+        pass
+
+
+class AudioDevice(_Device):
+    def __init__(
+            self,
+            id,
+            sampling_rate,
+            block_size,
+            dtype,
+            latency=None,
+            extra_settings=None,
+            # finished_callback=None,
+            clip_off=None,
+            dither_off=None,
+            never_drop_input=None,
+            prime_output_buffers_using_stream_callback=None
+            ):
+        super().__init__(id, sampling_rate, block_size, dtype)
+
+    @property
+    def stream():
+        pass
+
+    @staticmethod
+    def callback():
+        pass
+
+    def playback(data):
+        # fill queue, stream.start()
+        pass
+
+    def record(n_samples):
+        # stream start, read into the queue
+        pass
+
+    def playback_record(data):
+        # see combination above
+        pass
+
+    def initialize_playback(channels):
+        # init queue, define callback, init stream
+        pass
+
+    def initialize_record(channels):
+        pass
+
+    def initialize_playback_record(input_channels, output_channels):
+        pass
+
+    def abort():
+        # abort
+        pass
+
+    def close():
+        # remove stream
+        pass
---
+++ haiopy/io.py
@@ -0,0 +1,78 @@
+from . import devices
+
+
+class _AudioIO(object):
+    def __init__(
+            self,
+            device
+            ):
+        super().__init__()
+
+    def start():
+        pass
+
+    def stop():
+        pass
+
+    def wait():
+        pass
+
+
+class Playback(_AudioIO):
+    def __init__(
+            self,
+            device,
+            input_channels,
+            repetitions=1,
+            loop=False):
+        super().__init__(device=device)
+        self._output_signal = None
+
+    @property
+    def output_signal(self):
+        return self._output_signal
+
+    @output_signal.setter
+    def output_signal(self, sig):
+        self._output_signal = sig
+
+
+class Record(_AudioIO):
+    def __init__(
+            self,
+            device,
+            output_channels,
+            duration=None,
+            fft_norm='amplitude',
+            ):
+        super().__init__(device=device)
+        self._input_signal = None
+
+    @property
+    def input_signal(self):
+        return self._input_signal
+
+
+class PlaybackRecord(Playback, Record):
+    def __init__(
+            self,
+            device,
+            input_channels,
+            output_channels,
+            ):
+        super().__init__(
+            device=device,
+            input_channels=input_channels,
+            output_channels=output_channels)
+
+
+def playback():
+    pass
+
+
+def record():
+    pass
+
+
+def playback_record():
+    pass
