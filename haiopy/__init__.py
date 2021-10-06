1c869f47ccb45ad5fa170d557d4101f0915e5353
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-10-06 10:07:05 +0200

    add init AudioDevice

--- haiopy/__init__.py
+++ haiopy/__init__.py
@@ -3,3 +3,11 @@
 __author__ = """The pyfar developers"""
 __email__ = 'marco.berzborn@akustik.rwth-aachen.de'
 __version__ = '0.1.0'
+
+
+from devices import AudioDevice
+
+
+__all__ = [
+    'AudioDevice'
+]
