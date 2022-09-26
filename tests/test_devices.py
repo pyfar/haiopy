6a2d4c0b0290ffcc7cdb0ecb11607dd5df2b5dd8
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2022-09-26 11:15:32 +0200

    mocks for checking of input and output settings

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -6,3 +6,17 @@ from unittest.mock import patch
 @patch('sounddevice.query_devices', new=utils.query_devices)
 def test_audio_device():
     devices.AudioDevice(0)
+
+
+@patch('sounddevice.query_devices', new=utils.query_devices)
+@patch('sounddevice.check_input_settings', new=utils.check_input_settings)
+def test_check_input_settings():
+    in_device = devices.AudioInputDevice()
+    # in_device.check_settings()
+
+
+@patch('sounddevice.query_devices', new=utils.query_devices)
+@patch('sounddevice.check_output_settings', new=utils.check_output_settings)
+def test_check_output_settings():
+    in_device = devices.AudioOutputDevice()
+    # in_device.check_settings()
--- tests/utils.py
+++ tests/utils.py
@@ -40,3 +40,30 @@ def query_devices(id=None, kind=None):
             'default_high_output_latency': 0.15,
             'default_samplerate': 44100
         }
+
+
+def supported_mock_device_parameters():
+    return {
+        'samplerate': [44.1e3, 48e3, 2*44.1e3, 96e3, 192e3],
+        'dtype': ['float32'],
+        'channels': [8]}
+
+
+def check_output_settings(
+        device=None,
+        channels=None,
+        dtype=None,
+        extra_settings=None,
+        samplerate=None):
+    """So far this only passes for all settings"""
+    pass
+
+
+def check_input_settings(
+        device=None,
+        channels=None,
+        dtype=None,
+        extra_settings=None,
+        samplerate=None):
+    """So far this only passes for all settings"""
+    pass
