147d68bee751e02113b908deb0ae9e6ca7e590d9
Marco Berzborn <marco.berzborn@ditf.de>
2025-04-10 18:54:40 +0200

    init tests for mappings

---
+++ tests/test_mappings.py
@@ -0,0 +1,39 @@
+from unittest.mock import patch
+import pytest
+
+from haiopy.devices import (
+    OutputChannelMapping, InputChannelMapping)
+
+
+@pytest.mark.parametrize(
+    'valid_apis', [
+        'windows wasapi',
+        'windows directsound',
+        'asio',
+        'windows wdm-ks'])
+@patch('platform.system', new=lambda: 'Windows')
+@patch('sounddevice.AsioSettings', new=lambda channel_selectors: None)
+def test_init_checks_windows(valid_apis):
+    OutputChannelMapping([0], 1, valid_apis)
+    InputChannelMapping([0], 1, valid_apis)
+
+
+@pytest.mark.parametrize(
+    'valid_apis', [
+        'alsa',
+        'oss',
+        'pulse',
+        'jack'])
+@patch('platform.system', new=lambda: 'Linux')
+def test_init_checks_linux(valid_apis):
+    OutputChannelMapping([0], 1, valid_apis)
+    InputChannelMapping([0], 1, valid_apis)
+
+
+@pytest.mark.parametrize(
+    'valid_apis', ['coreaudio'])
+@patch('platform.system', new=lambda: 'Darwin')
+@patch('sounddevice.CoreAudioSettings', new=lambda channel_map: None)
+def test_init_checks_macos(valid_apis):
+    OutputChannelMapping([0], 1, valid_apis)
+    InputChannelMapping([0], 1, valid_apis)
